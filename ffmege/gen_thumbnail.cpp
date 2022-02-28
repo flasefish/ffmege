e27b1fc6c640aab5ba0bc4f88a2243c7d3b6480a
denver.zhou <flasefish@protonmail.com>
2022-02-28 20:31:48 +0800

    add file

---
+++ ffmege/gen_thumbnail.cpp
@@ -0,0 +1,11 @@
+#include "gen_thumbnail.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
---
+++ ffmege/gen_thumbnail.h
@@ -0,0 +1,2 @@
+#pragma once
+int gen_thumbnail(const char* formatname, const int width, void* data, int data_size, void* outbuff, int outbufflen, int *outsz);
